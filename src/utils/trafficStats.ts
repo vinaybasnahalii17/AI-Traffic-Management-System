bb2b709c0849b938d16af2e42c5be18daf8b45d5
vinay <vinay.s@s.amity.edu>
2026-06-19 21:34:03 +0530

    add average vehicle calculation

--- src/utils/trafficStats.ts
+++ src/utils/trafficStats.ts
@@ -6,3 +6,12 @@ export function getTotalVehicles(
 ): number {
   return north + east + south + west;
 }
+
+export function getAverageVehicles(
+  north: number,
+  east: number,
+  south: number,
+  west: number
+): number {
+  return (north + east + south + west) / 4;
+}
