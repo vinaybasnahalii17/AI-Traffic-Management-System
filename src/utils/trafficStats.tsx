3df3ce2f9b1995db5db916a0765a6c94efc623af
vinay <vinay.s@s.amity.edu>
2026-06-19 21:47:38 +0530

    create traffic statistics component, display total vehicle count and average vehicle count

---
+++ src/utils/trafficStats.tsx
@@ -0,0 +1,19 @@
+export default function TrafficStats() {
+  return (
+    <div>
+      <h2>Traffic Statistics</h2>
+    </div>
+  );
+}
+
+export default function TrafficStats() {
+  return (
+    <div>
+      <h2>Traffic Statistics</h2>
+
+      <p>Total Vehicles: 0</p>
+    </div>
+  );
+}
+
+<p>Average Vehicles Per Lane: 0</p>
