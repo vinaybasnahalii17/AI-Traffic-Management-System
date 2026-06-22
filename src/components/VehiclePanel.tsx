d436738285ee27c260c0e6cc0e4aa8cd0a1d7504
vinay <vinay.s@s.amity.edu>
2026-06-22 17:07:25 +0530

    add vehicle position display

--- src/components/VehiclePanel.tsx
+++ src/components/VehiclePanel.tsx
@@ -4,6 +4,7 @@ export default function VehiclePanel() {
       <h2>Vehicle Tracking Panel</h2>
       <p>Vehicle Type: Car</p>
       <p>Lane: North</p>
+      <p>Position: 0.32</p>
     </div>
   );
 }
